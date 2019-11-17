0e6ea4f34cdf0d209f24eb10b0200fa4ea4b55a0
Nick White <whitenj2@cardiff.ac.uk>
2019-11-17 21:12:37 +0000

    Jinja inheritance now live with homepage serving as website template. Changed status on Taiga

--- chc_server.py
+++ chc_server.py
@@ -11,12 +11,18 @@ serv = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'css'])
 
-@serv.route("/test", methods = ['GET'])
-def main_header():
+@serv.route("/home", methods = ['GET'])
+def frontPage():
     if request.method == 'GET':
 
-        # os.chdir('..')
         return render_template('01_homepage.html')
+
+@serv.route("/contact", methods = ['GET'])
+def contactUs():
+    if request.method == 'GET':
+
+        return render_template('02-contact_us.html')
+
 
 if __name__ == "__main__":
     serv.run(debug=True)
