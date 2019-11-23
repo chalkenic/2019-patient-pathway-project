2daa967413578e36154746195f46709b2b7edffa
Nick White <whitenj2@cardiff.ac.uk>
2019-11-23 23:44:40 +0000

    Added feature for user to log in from any page via python button name validation.

--- chc_server.py
+++ chc_server.py
@@ -18,30 +18,18 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'css'])
 @serv.route("/home", methods = ['POST','GET'])
 def frontPage():
     if request.method == 'GET':
-        # username = "Admin"
+        username = request.cookies.get('username')
         return render_template('00_homepage.html')
     if request.method == 'POST':
-        username = request.form.get('user_email', default="Error")
-        tmp = username.split('@')
-        user = tmp[0]
-        password = request.form.get('user_password', default="Error")
-        if login_credentials(username, password) == True:
-            response = make_response(render_template('00_homepage.html',username = user, login_message = 'hello ' + user))
 
-            response.set_cookie('username', user )
-
-            session['user_email'] = request.form['user_email']
-            session['Password'] = request.form['user_password']
-            print("Password checks out, hello " + user + "!")
+        if 'login1' in request.form:
+            return user_login()
 
         else:
-            response = make_response(render_template('00_homepage.html', login_message ='Incorrect login, please try again', username=""))
-        return response
-    else:
-        username = ''
-        if 'username' in session:
-            username = escape(session['username'])
-        return render_template('00_homepage.html', login_message ='', username = 'None')
+            username = ''
+            if 'username' in session:
+                username = escape(session['username'])
+            return render_template('00_homepage.html', login_message ='', username = 'None')
 
 def login_credentials(username, password):
         if username != None:
@@ -52,29 +40,92 @@ def login_credentials(username, password):
         else:
             return False
 
-@serv.route("/section", methods = ['GET'])
+def user_login():
+    username = request.form.get('user_email', default="Error")
+    tmp = username.split('@')
+    user = tmp[0]
+    password = request.form.get('user_password', default="Error")
+    if login_credentials(username, password) == True:
+        response = make_response(render_template('00_homepage.html',username = user, login_message = 'hello ' + user))
+
+        response.set_cookie('username', user )
+
+        session['user_email'] = request.form['user_email']
+        session['Password'] = request.form['user_password']
+        print("Password checks out, hello " + user + "!")
+
+    else:
+        response = make_response(render_template('00_homepage.html', login_message ='Incorrect login, please try again', username=""))
+    return response
+
+@serv.route("/section", methods = ['POST','GET'])
 def section():
     if request.method == 'GET':
         username = request.cookies.get('username')
         return render_template('01-p_section.html', username = username)
 
-@serv.route("/survey", methods = ['GET'])
+    if request.method == 'POST':
+
+        if 'login1' in request.form:
+            return user_login()
+
+        else:
+            username = ''
+            if 'username' in session:
+                username = escape(session['username'])
+            return render_template('00_homepage.html', login_message ='', username = 'None')
+
+@serv.route("/survey", methods = ['POST','GET'])
 def survey():
     if request.method == 'GET':
+        username = request.cookies.get('username')
+        return render_template('03-survey.html', username = username)
 
-        return render_template('03-survey.html')
 
-@serv.route("/LTS-surv", methods = ['GET'])
+    if request.method == 'POST':
+
+        if 'login1' in request.form:
+            return user_login()
+
+        else:
+            username = ''
+            if 'username' in session:
+                username = escape(session['username'])
+            return render_template('00_homepage.html', login_message ='', username = 'None')
+
+@serv.route("/LTS-surv", methods = ['POST','GET'])
 def LTS_surv():
     if request.method == 'GET':
+        username = request.cookies.get('username')
+        return render_template('04-Local&ThirdSector.html', username = username)
 
-        return render_template('04-Local&ThirdSector.html')
+    if request.method == 'POST':
+        if 'login1' in request.form:
+            return user_login()
 
-@serv.route("/contact", methods = ['GET'])
+        else:
+            username = ''
+            if 'username' in session:
+                username = escape(session['username'])
+            return render_template('00_homepage.html', login_message ='', username = 'None')
+
+
+@serv.route("/contact", methods = ['POST','GET'])
 def contactUs():
     if request.method == 'GET':
+        username = request.cookies.get('username')
+        return render_template('02-contact_us.html', username = username)
 
-        return render_template('02-contact_us.html')
+    if request.method == 'POST':
+        if 'login1' in request.form:
+            return user_login()
+
+        else:
+            username = ''
+            if 'username' in session:
+                username = escape(session['username'])
+            return render_template('00_homepage.html', login_message ='', username = 'None')
+
 
 
 # /* TEST STUFF!!! */
