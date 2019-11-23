8c38ddd7576770f09057359020e38cf61fdc391b
Abdi Omar <OmarA7@cardiff.ac.uk>
2019-11-23 19:51:45 +0000

    moved js from survey to main js folder

--- static/js-main.js
+++ static/js-main.js
@@ -6,3 +6,24 @@
 //     x.className = "topnav";
 //   }
 // }
+// function myFunction() {
+//   alert("Thank you for filling in this survey");
+// }
+// </script>
+// <script type="text/javascript">
+//  function validateForm(){
+//    var completion date = document.getElementById('completion date').value;
+//    var typeofcancer = document.getElementById('typeofcancer').value;
+//    var duration spent receiving treatment = document.getElementById('duration spent receiving treatment').value;
+//     var impact nhs has had on your recovery = document.getElementById('impact nhs has had on your recovery').value;
+//      var affects on social life = document.getElementById('affects on social life').value;
+//    if(completion date, typeofcancer, duration spent receiving treatment, impact nhs has had on your recovery=='')
+// {
+//  alert('Please Fill all Required Fields');
+//  return false;
+// }
+// else {
+// return true;
+//  }
+// }
+// </script>
