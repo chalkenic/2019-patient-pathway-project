5e0cb03566aeb197b76795b4a636379cf2da68bd
Nick White <whitenj2@cardiff.ac.uk>
2019-11-30 17:27:11 +0000

    Further testing of json files into js-main script.

--- static/js-main0.js
+++ static/js-main0.js
@@ -1,21 +1,39 @@
-// function ChangeBrand() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.open("GET", '/home', true);
-//   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+// function getCookie(name) {
+//   var cookieArr = document.cookie.split(";");
 //
-//   xhttp.onreadystatechange = function() {
-//     if (xhttp.readyState === 4) {
-//       if (xhttp.status === 200) {
-//         // var text = "Admin"
-//         var text = xhttp.responseText
-//         document.getElementById("user").innerHTML = text;
-//       } else {
-//         console.error(xhttp.statusText);
-//       }
+//   for (var i = 0; i <cookieArr.length; i++) {
+//     var cookiePair = cookieArr[i].split("=");
+//
+//     if (name == cookiePair[0].trim()) {
+//       return decodeURIComponent(cookiePair[1]);
 //     }
-//   };
-//   xhttp.send()
-// };
+//   }
+// }
+
+// document.write(getCookie("email_addr"))
+
+
+function callAjax(url)
+{
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function()
+  {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+    {
+        var data = JSON.parse(xmlhttp.responseText);
+
+        document.getElementById("test").innerHTML = data.email_addr;
+    }
+  }
+  xmlhttp.open("GET", "line_graph_data.txt", true);
+  xmlhttp.send();
+}
+
+function cb_myCallback (theStuff)
+{
+  console.log(theStuff)
+}
 
 // TEST STUFF
 // TEST STUFF
@@ -27,19 +45,20 @@
 // TEST STUFF
 // TEST STUFF
 
-var time = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
 // For drawing the lines
-var gen_happ = [86,114,106,106,107,111,133,221,783,2478];
+var gen_happ = l_values;
 var int_happ = [6,3,2,2,7,26,82,172,312,433];
 
 var ctx = document.getElementById("userChart");
 var userChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: time,
+    labels: [{% for item in l_labels %}
+      "{{item}}",
+      {% endfor %}]
     datasets: [
       {
-        data: gen_happ,
+        data: {{"1_values"}},
         label: "General happiness",
         borderColor: "#3e95cd",
         fill: false
