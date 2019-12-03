ff53cd7e031d00ff60276d387f438ce26fc43689
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2019-12-03 14:49:11 +0000

    SQL updated for contact users containing query and ID fields only

--- test/hamid/contact_us_users.sql
+++ test/hamid/contact_us_users.sql
@@ -1,7 +1,4 @@
 CREATE TABLE IF NOT EXISTS 'contactFormUsers' (
     'ID' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    'firstName' TEXT NOT NULL,
-    'lastName' TEXT NOT NULL,
-    'email' TEXT NOT NULL,
     'query' TEXT NOT NULL
 );
