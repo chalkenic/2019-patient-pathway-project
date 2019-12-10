0d5baaa404f449e6536045856fbfa8c21dcbf17d
Nikrad Nassiri <nassirin@cardiff.ac.uk>
2019-12-10 01:08:10 +0000

    Revert "Register page changes"
    This reverts commit ab8a7d127c5aa37407bf3dc71708693f562ed3fe

--- chc_server.py
+++ chc_server.py
@@ -72,77 +72,41 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'css'])
 #         return msg
 #         conn.close()
 
-# ### working
-# @serv.route("/register", methods = ['POST', 'GET'])
-# def newUser():
-#     if request.method == 'GET':
-#         return render_template('register.html')
-#     elif request.method == 'POST':
-#         regEmail = request.form.get('registerEmail', default = 'error')
-#         regPass1 = request.form.get ('registerPassword', default = 'error')
-#
-#         userID = "153"
-#         userName = "Nikrad"
-#         userAccess = "User"
-#         volunteerID = "1323"
-#
-#         print (regEmail)
-#         print (regPass1)
-#         try:
-#             # request.form.get('')
-#             conn = sqlite3.connect(DATABASE)
-#             cur = conn.cursor()
-#             # cur.execute(("INSERT INTO accounts('email_addr','password') VALUES ('','');"),(regEmail, regPass1))
-#             # cur.execute("INSERT INTO 'accounts' ('email_addr','password') VALUES ('152', ?, '', ?, 'User','1512');"),(regEmail, regPass1)
-#
-#             cur.execute(("INSERT INTO accounts('userID', 'email_addr', 'name', 'password', 'access', 'volunteerID') VALUES (?,?,?,?,?,?);"),(userID, regEmail, userName, regPass1, userAccess, volunteerID))
-#
-#
-#             conn.commit()
-#             msg ="Survey Data successfully recorded"
-#             conn.close()
-#         except:
-#             conn.rollback()
-#             msg ="Error in appending data "
-#         finally:
-#             return msg + regEmail
-#             conn.close()
-
 
 @serv.route("/register", methods = ['POST', 'GET'])
 def newUser():
     if request.method == 'GET':
         return render_template('register.html')
-
     elif request.method == 'POST':
-        regName = request.form.get('registerName', default = 'error')
         regEmail = request.form.get('registerEmail', default = 'error')
         regPass1 = request.form.get ('registerPassword', default = 'error')
-        regPass2 = request.form.get ('registerPassword2', default = 'error')
-        if regPass1 == regPass2:
-            userAccess = "User"
-            volunteerID = "9925"
 
-            print (regEmail)
-            print (regPass1)
-            try:
-                conn = sqlite3.connect(DATABASE)
-                cur = conn.cursor()
-                cur.execute(("INSERT INTO accounts('email_addr', 'name', 'password', 'access', 'volunteerID') VALUES (?,?,?,?,?);"),(regEmail, regName, regPass1, userAccess, volunteerID))
-                conn.commit()
-                msg ="Survey Data successfully recorded"
-                conn.close()
-            except:
-                conn.rollback()
-                msg ="Error in appending data "
-            finally:
-                return msg + regEmail
-                conn.close()
-        else:
-            return "didnt match"
+        userID = "15"
+        userName = "Nikrad"
+        userAccess = "User"
+        volunteerID = "132"
+
+        print (regEmail)
+        print (regPass1)
+        try:
+            # request.form.get('')
+            conn = sqlite3.connect(DATABASE)
+            cur = conn.cursor()
+            # cur.execute(("INSERT INTO accounts('email_addr','password') VALUES ('','');"),(regEmail, regPass1))
+            # cur.execute("INSERT INTO 'accounts' ('email_addr','password') VALUES ('152', ?, '', ?, 'User','1512');"),(regEmail, regPass1)
+
+            cur.execute(("INSERT INTO accounts('userID', 'email_addr', 'name', 'password', 'access', 'volunteerID') VALUES (?,?,?,?,?,?);"),(userID, regEmail, userName, regPass1, userAccess, volunteerID))
 
 
-
+            conn.commit()
+            msg ="Survey Data successfully recorded"
+            conn.close()
+        except:
+            conn.rollback()
+            msg ="Error in appending data "
+        finally:
+            return msg + regEmail
+            conn.close()
 
 
 
