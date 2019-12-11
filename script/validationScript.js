6e75a5c928afd0e8eb02f15910b41172f115769f
Nicholas White <whitenj2@cardiff.ac.uk>
2019-12-11 23:30:45 +0000

    Delete validationScript.js

--- script/validationScript.js
+++
@@ -1,8 +0,0 @@
-var username = document.getElementById("usernameInput").value;
-var password = document.getElementById("passwordInput").value;
-
-var lowerCaseLetters = /[a-z]/g;
-var upperCaseLetters = /[A-Z]/g;
-var numbers = /[0-9]/g;
-
-if (username.value.match(lowerCaseLetters)) AND (username.value.match(upperCaseLetters))
